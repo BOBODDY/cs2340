d5003c239766cbd9f753387efae77fab75af9d5f
Nick Mathews <nickel3956@gmail.com>
2014-03-07 12:28:21 -0500

    added button to create report. does nothing for now

--- src/com/so/sofinances/UserHomeActivity.java
+++ src/com/so/sofinances/UserHomeActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -65,6 +66,17 @@ public class UserHomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.user_home, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_report:
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 	
 	public void onCreateClick(View v){
