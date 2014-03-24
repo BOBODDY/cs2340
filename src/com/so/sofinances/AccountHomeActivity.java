3d91de10299d618dd4da6d7bb5c077a0b5271f60
kodylaseter <kodylaseter1@gmail.com>
2014-03-24 12:17:48 -0400

    all of this seems to work

--- src/com/so/sofinances/AccountHomeActivity.java
+++ src/com/so/sofinances/AccountHomeActivity.java
@@ -41,7 +41,7 @@ public class AccountHomeActivity extends Activity {
 			accountName = "";
 		}
 		balance = (TextView) findViewById(R.id.transactionBalance); // TODO; shows 0 balance when no transactions are present 
-		
+		balance.setText("Balance: " + UserHandler.getAccount(accountName).getBalanceString());
 		transactionHistory = (ListView) findViewById(R.id.transactionHistory);
 		
 		List<Transaction> transacts = UserHandler.getAccount(accountName).getTransactions();
