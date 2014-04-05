31130bbd9c93adb724b6f422ff10388b3bdc27eb
bshum3 <bshum3@gatech.edu>
2014-04-04 21:11:14 -0400

    JUnit for User
    JUnit Test for getAccount(String accountName) in User

---
+++ src/com/so/sofinances/model/test/UserTest.java
@@ -0,0 +1,22 @@
+package com.so.sofinances.model.test;
+import junit.framework.TestCase;
+import com.so.sofinances.model.User;
+
+public class UserTest extends TestCase{
+	
+	public void testFoundAccount(){
+		User user = new User("Fn Ln", "user1", "password");
+		user.addAccount("name", "acc1", 100, 0);
+		assertTrue(user.getAccount("acc1").getDisplayName() == "acc1");
+		assertTrue(user.getAccount("acc1").getBalance() == 100);
+		assertTrue(user.getAccount("acc1").getFullName() == "name");
+		assertTrue(user.getAccount("acc1").getMonthlyInterestRate() == 0);
+	}
+	
+	public void testNoAccountFound(){
+		User user = new User("Fn Ln", "user1", "password");
+		user.addAccount("name", "acc1", 100, 0);
+		assertTrue(user.getAccount("acc2") == null);
+	}
+
+}
