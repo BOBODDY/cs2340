8d99b3d238889289ac5d7101a92ea854db792811
Nick Mathews <nickel3956@gmail.com>
2014-04-09 12:03:35 -0400

    had to update test when compareTo was changed

--- src/com/so/sofinances/model/test/TimeDataTest.java
+++ src/com/so/sofinances/model/test/TimeDataTest.java
@@ -17,48 +17,48 @@ public class TimeDataTest extends TestCase {
 		TimeData time1 = new TimeData(5,12,2012);
 		TimeData time2 = new TimeData(6,20,2006);
 		
-		assertTrue(time1.compareTo(time2) < 0);
+		assertTrue(time1.compareTo(time2) > 0);
 	}
 	
 	public void testYearBefore() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(4,4,2013);
 		
-		assertTrue(time1.compareTo(time2) < 0);
+		assertTrue(time1.compareTo(time2) > 0);
 	}
 	
 	public void testYearAfter() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(4,4,2015);
 		
-		assertTrue(time1.compareTo(time2) > 0);
+		assertTrue(time1.compareTo(time2) < 0);
 	}
 	
 	public void testMonthBefore() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(3,4,2014);
 		
-		assertTrue(time1.compareTo(time2) < 0);
+		assertTrue(time1.compareTo(time2) > 0);
 	}
 	
 	public void testMonthAfter() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(5,4,2014);
 		
-		assertTrue(time1.compareTo(time2) > 0);
+		assertTrue(time1.compareTo(time2) < 0);
 	}
 	
 	public void testDayBefore() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(4,3,2014);
 		
-		assertTrue(time1.compareTo(time2) < 0);
+		assertTrue(time1.compareTo(time2) > 0);
 	}
 	
 	public void testDayAfter() {
 		TimeData time1 = new TimeData(4,4,2014);
 		TimeData time2 = new TimeData(4,5,2014);
 		
-		assertTrue(time1.compareTo(time2) > 0);
+		assertTrue(time1.compareTo(time2) < 0);
 	}
 }
